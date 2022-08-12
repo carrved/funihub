7991d4d76a74554309dfd290d868b806a716b090
z4xi <36055611+z4xi@users.noreply.github.com>
2022-08-12 16:18:49 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 9397663086 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/funihub/main/ninja_training_sim.lua"))()
 elseif game.PlaceId == 155615604 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/funihub/main/prison_life.lua?token=GHSAT0AAAAAABXS7QVFXNSG3STBT74TRIQ2YXWYN7Q"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/z4xi/funihub/main/prison_life.lua"))()
 end
